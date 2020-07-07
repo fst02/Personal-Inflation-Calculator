3bef9814f8291f21348f24a9e004311301030af1
bh-fst02 <bh.fst02@gmail.com>
2020-07-07 15:35:59 +0200

    Implement signin endpoint

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const CategoryController = require('../controllers/CategoryController');
 const RegistrationController = require('../controllers/RegistrationController');
+const SignInController = require('../controllers/SignInController');
 
 const router = express.Router();
 
 router.get('/categories', CategoryController.getAll);
 router.post('/registration', RegistrationController.register);
 router.get('/registration/verify', RegistrationController.verify);
+router.post('/signIn', SignInController.signIn);
 
 module.exports = router;
